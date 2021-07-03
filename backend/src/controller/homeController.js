3d9b4d89dc0542abc92eafaeabdd63fa2e36992f
stefanyds <ste.stefany22@gmail.com>
2021-07-03 12:15:15 -0300

    feat: updated HomeController to create a new student

--- backend/src/controller/homeController.js
+++ backend/src/controller/homeController.js
@@ -1,9 +1,17 @@
+import Student from '../model/student';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
-      mensagem: 'Olá Mundo!',
+  async index(req, res) {
+    const newStudent = await Student.create({
+      name: 'Devmaster',
+      lastname: 'Programmer Hero',
+      email: 'devmaster@programmerhero.com.br',
+      age: 21,
+      weight: 99,
+      height: 1.67,
     });
+    // para exibir o que for adicionado
+    res.json(newStudent);
   }
 }
 
