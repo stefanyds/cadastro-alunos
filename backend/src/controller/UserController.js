2e7f54b54367c18ed948b0698abfde45eaf96243
stefanyds <ste.stefany22@gmail.com>
2021-07-31 12:34:12 -0300

    feat: created CRUD for users

--- backend/src/controller/UserController.js
+++ backend/src/controller/UserController.js
@@ -4,9 +4,73 @@ class UserController {
   async create(request, response) {
     try {
       const newUser = await User.create(request.body);
-      response.json(newUser);
+      return response.json(newUser);
     } catch (e) {
-      response.status(400).json({
+      return response.status(400).json({
+        errors: e.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async index(request, response) {
+    try {
+      const users = await User.findAll();
+      if (users) {
+        return response.json(users);
+      }
+      // nesse caso se  não colocarmos isso, ele vai retornar algo nulo
+      throw new Error();
+    } catch (e) {
+      return response.status(200).json('Nenhum usuário cadastrado');
+    }
+  }
+
+  async show(request, response) {
+    try {
+      const user = await User.findByPk(request.params.id);
+      if (user) {
+        return response.json(user);
+      }
+      return response.json({ errors: 'Usuário não encontrado' });
+    } catch (e) {
+      return response.status(500).json({ errors: 'Ops! Ocorreu um erro. Tente novamente mais tarde.' });
+    }
+  }
+
+  async update(request, response) {
+    try {
+      if (!request.params.id) {
+        return response.status(400).json({ errors: 'ID não enviado.' });
+      }
+
+      const user = await User.findByPk(request.params.id);
+      if (!user) {
+        return response.status(400).json({ errors: 'Usuário não encontrado' });
+      }
+      const updatedUser = await user.update(request.body);
+      // return response.status(204);
+      return response.json(updatedUser);
+    } catch (e) {
+      return response.status(400).json({
+        errors: e.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async delete(request, response) {
+    try {
+      if (!request.params.id) {
+        return response.status(400).json({ errors: 'ID não enviado.' });
+      }
+
+      const user = await User.findByPk(request.params.id);
+      if (!user) {
+        return response.status(400).json({ errors: 'Usuário não encontrado' });
+      }
+      await user.destroy();
+      return response.json(user);
+    } catch (e) {
+      return response.status(400).json({
         errors: e.errors.map((error) => error.message),
       });
     }
@@ -14,3 +78,16 @@ class UserController {
 }
 
 export default new UserController();
+
+/*
+try {
+      const users = await User.findAll();
+      if (users) {
+        return response.json(users);
+      }
+      return response.json('Nenhum usuário cadastrado');
+    } catch (e) {
+      return response.status(500).json('Ops! Ocorreu um erro, tente novamente mais tarde');
+    }
+  }
+  */
--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -3,6 +3,10 @@ import UserController from '../controller/UserController';
 
 const router = new Router();
 router.post('/', UserController.create);
+router.get('/', UserController.index);
+router.get('/:id', UserController.show);
+router.put('/:id', UserController.update);
+router.delete('/:id', UserController.delete);
 
 export default router;
 
