4a2ffad857b3981feae3ea215a905a30ccbbccfd
stefanyds <ste.stefany22@gmail.com>
2021-10-30 11:37:16 -0300

    feat: created student CRUD

--- backend/src/controller/StudentController.js
+++ backend/src/controller/StudentController.js
@@ -1,6 +1,18 @@
 import Student from '../model/Student';
 
 class StudentController {
+  async create(req, res) {
+    try {
+      const student = await Student.create(req.body);
+
+      return res.json(student);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((error) => error.message),
+      });
+    }
+  }
+
   async index(req, res) {
     try {
       const students = await Student.findAll();
@@ -9,9 +21,92 @@ class StudentController {
         return res.json(students);
       }
       throw new Error(); // faz cair no catch
-    } catch (error) {
+    } catch (e) {
       return res.status(400).json({
-        errors: ['Nenhum aluno cadastrado'],
+        errors: e.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Id não informado'],
+        });
+      }
+
+      const student = await Student.findByPk(id);
+
+      if (!student) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+
+      return res.json(student);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Id não informado'],
+        });
+      }
+
+      const student = await Student.findByPk(id);
+
+      if (!student) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+      // conferir se está recebendo o body
+
+      const updateStudent = await student.update(req.body);
+
+      return res.json(updateStudent);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Id não informado'],
+        });
+      }
+
+      const student = await Student.findByPk(id);
+
+      if (!student) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+
+      await student.destroy();
+
+      return res.json({ deleted: true });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((error) => error.message),
       });
     }
   }
--- backend/src/routes/studentRouter.js
+++ backend/src/routes/studentRouter.js
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import StudentController from '../controller/StudentController';
+import loginRequired from '../middleware/loginRequired';
 
 const router = new Router();
 
 router.get('/', StudentController.index);
+router.get('/:id', StudentController.show);
+
+router.post('/', loginRequired, StudentController.create);
+router.put('/:id', loginRequired, StudentController.update);
+router.delete('/:id', loginRequired, StudentController.delete);
 
 export default router;
