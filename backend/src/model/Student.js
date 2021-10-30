765161bf3878d94793307a7766b9e99352e8d7e1
stefanyds <ste.stefany22@gmail.com>
2021-10-30 10:21:48 -0300

    feat: added student model validates

--- backend/src/model/Student.js
+++ backend/src/model/Student.js
@@ -3,12 +3,64 @@ import Sequelize, { Model } from 'sequelize';
 export default class Student extends Model {
   static init(sequelize) {
     super.init({
-      name: Sequelize.STRING,
-      lastname: Sequelize.STRING,
-      email: Sequelize.STRING,
-      age: Sequelize.INTEGER,
-      weight: Sequelize.FLOAT,
-      height: Sequelize.FLOAT,
+      name: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'O nome precisa ter entre 3 e 255 caracteres',
+          },
+        },
+      },
+      lastname: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'O sobrenome precisa ter entre 3 e 255 caracteres',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        isEmail: {
+          msg: 'Email inválido',
+        },
+        unique: {
+          name: 'students.email', // bug no sequelize, deve informar sempre: nometabela.campo
+          msg: 'Email já existente',
+        },
+      },
+      age: {
+        type: Sequelize.INTEGER,
+        defaultValue: '',
+        validate: {
+          isInt: {
+            msg: 'Idade precisa ser um número inteiro',
+          },
+        },
+      },
+      weight: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Peso precisa ser um número inteiro ou de ponto flutuante',
+          },
+        },
+      },
+      height: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Altura precisa ser um número inteiro ou de ponto flutuante',
+          },
+        },
+      },
     }, {
       sequelize,
     });
