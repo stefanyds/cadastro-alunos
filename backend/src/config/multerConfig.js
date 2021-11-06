de3dcd156303c8f41a9c6865134d599b36fd2142
stefanyds <ste.stefany22@gmail.com>
2021-11-06 11:56:21 -0300

    feat: updated to refuse invalid image files

--- backend/src/config/multerConfig.js
+++ backend/src/config/multerConfig.js
@@ -2,6 +2,13 @@ import multer from 'multer';
 import { extname, resolve } from 'path';
 
 export default {
+  fileFilter: (req, file, callback) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') // foi um E pq nesse caso o OU é assim
+    {
+      return callback(new multer.MulterError('O arquivo precisa ser do tipo PNG ou JPG.'));
+    }
+    return callback(null, true);
+  },
   storage: multer.diskStorage({
     destination: (req, file, callback) => {
       callback(null, resolve(__dirname, '..', '..', 'uploads'));
