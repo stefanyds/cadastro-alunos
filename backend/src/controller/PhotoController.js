2b9fdf351e3730f63117d4657da20cf6bca53188
stefanyds <ste.stefany22@gmail.com>
2021-11-06 12:10:50 -0300

    feat: updated to return error message into JSON

--- backend/src/controller/PhotoController.js
+++ backend/src/controller/PhotoController.js
@@ -1,6 +1,18 @@
+import multer from 'multer';
+import multerConfig from '../config/multerConfig';
+
+const uploadMiddleware = multer(multerConfig).single('photo');
+
 class PhotoController {
   async store(req, res) {
-    res.json(req.file);
+    return uploadMiddleware(req, res, (error) => {
+      if (error) {
+        return res.status(400).json({
+          errors: [error.code],
+        });
+      }
+      return res.json(req.file);
+    });
   }
 }
 
--- backend/src/routes/photoRouter.js
+++ backend/src/routes/photoRouter.js
@@ -1,11 +1,7 @@
 import { Router } from 'express';
-import multer from 'multer';
-import multerConfig from '../config/multerConfig';
 import PhotoController from '../controller/PhotoController';
 
-const uploadMiddleware = multer(multerConfig);
-
 const router = new Router();
-router.post('/', uploadMiddleware.single('photo'), PhotoController.store);
+router.post('/', PhotoController.store);
 
 export default router;
