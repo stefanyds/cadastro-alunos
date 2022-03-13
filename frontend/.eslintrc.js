d3eb0f784e2a4e2f099d5853085ed94b86fc7623
stefanyds <ste.stefany22@gmail.com>
2022-03-13 10:41:35 -0300

    chore: added configuration into .eslintrc.js

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,21 +2,27 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parser: "@babel/eslint-parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    requireConfigFile: false,
+    sourceType: "module",
+    babelOption: {
+      presets: ["@babel/preset-react"],
+    },
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ["react", "prettier", "react-hooks"],
   rules: {
+    "prettier/prettier": "error",
+    "import/prefer-default-export": "off",
+    "react/react-in-jsx-scope": "off",
+    "react-hooks/rules-of-hooks": "error", // Verifica as regras dos Hooks
+    "react-hooks/exhaustive-deps": "warn", // Verifica as dependências de effects
   },
 };
