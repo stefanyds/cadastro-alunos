2f99c31b3dcb2d5381636a50de42ab8ffbd43219
stefanyds <ste.stefany22@gmail.com>
2022-03-13 11:45:05 -0300

    feat: created Login component and style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
+import Login from './pages/Login';
+
 function App() {
-  return <h1>Cadastro de alunos</h1>;
+  return <Login />;
 }
 
 export default App;
---
+++ frontend/src/pages/Login/index.jsx
@@ -0,0 +1,5 @@
+import { Title } from './styled';
+
+export default function Login() {
+  return <Title isRed={false}>Login</Title>;
+}
