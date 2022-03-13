4fb44fd994dbf3d59b7b92e89d3b7f195f17581a
stefanyds <ste.stefany22@gmail.com>
2022-03-13 11:07:06 -0300

    refactor: updated configuration for ESlint and Prettier

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -4,25 +4,25 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier"],
-  parser: "@babel/eslint-parser",
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
     requireConfigFile: false,
-    sourceType: "module",
-    babelOption: {
-      presets: ["@babel/preset-react"],
+    sourceType: 'module',
+    babelOptions: {
+      presets: ['@babel/preset-react'],
     },
   },
-  plugins: ["react", "prettier", "react-hooks"],
+  plugins: ['react', 'prettier', 'react-hooks'],
   rules: {
-    "prettier/prettier": "error",
-    "import/prefer-default-export": "off",
-    "react/react-in-jsx-scope": "off",
-    "react-hooks/rules-of-hooks": "error", // Verifica as regras dos Hooks
-    "react-hooks/exhaustive-deps": "warn", // Verifica as dependências de effects
+    'prettier/prettier': 'error',
+    'import/prefer-default-export': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error', // Verifica as regras dos Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Verifica as dependências de effects
   },
 };
