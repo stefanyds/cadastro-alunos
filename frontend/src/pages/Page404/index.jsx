30e3680686861e0d6cea40f596d5fdd97751a3ca
stefanyds <ste.stefany22@gmail.com>
2022-03-20 11:47:18 -0300

    feat: created Page404

---
+++ frontend/src/pages/Page404/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from '../../styles/GlobalStyles';
+
+export default function Page404() {
+  return (
+    <Container>
+      <h1>Página não encontrada</h1>
+    </Container>
+  );
+}
--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from '../pages/Login';
+import Page404 from '../pages/Page404';
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="*" element={<Page404 />} />
     </Routes>
   );
 }
