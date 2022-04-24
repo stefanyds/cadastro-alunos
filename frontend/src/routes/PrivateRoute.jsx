c28640d809ce4e17fbebe124be026dd08b8694bb
stefanyds <ste.stefany22@gmail.com>
2022-04-24 11:09:31 -0300

    feat: created PrivateRoute component

---
+++ frontend/src/routes/PrivateRoute.jsx
@@ -0,0 +1,22 @@
+import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export default function PrivateRoute({ component: Component, isClosed }) {
+  const isLoggedIn = false;
+
+  if (isClosed && !isLoggedIn) {
+    return <Navigate to="/" />;
+  }
+
+  return <Component />;
+}
+
+PrivateRoute.defaultProps = {
+  isClosed: false,
+};
+
+PrivateRoute.propTypes = {
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+  isClosed: PropTypes.bool,
+};
--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from '../pages/Login';
 import Page404 from '../pages/Page404';
+import PrivateRoute from './PrivateRoute';
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="*" element={<Page404 />} />
+      <Route path="/" element={<PrivateRoute component={Login} />} />
+      <Route path="*" element={<PrivateRoute component={Page404} isClosed />} />
     </Routes>
   );
 }
