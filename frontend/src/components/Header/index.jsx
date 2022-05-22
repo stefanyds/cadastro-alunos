b5fe556710c6a69c40f5851e3ba4fb0002462c69
stefanyds <ste.stefany22@gmail.com>
2022-05-22 17:54:09 -0300

    feat: using useSelector into Header component

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,19 +1,28 @@
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { Nav } from './styles';
 
 export default function Header() {
+  const usuarioLogado = useSelector((state) => state.usuarioLogado);
+
   return (
     <Nav>
       <Link to="/">
         <FaHome size={24} />
       </Link>
-      <Link to="/user">
-        <FaUserAlt size={24} />
-      </Link>
-      <Link to="/exit">
-        <FaSignInAlt size={24} />
-      </Link>
+      {usuarioLogado ? (
+        <Link to="/user">
+          <FaUserAlt size={24} />
+        </Link>
+      ) : (
+        <Link to="/exit">
+          <FaSignInAlt size={24} />
+        </Link>
+      )}
+      <span style={{ color: '#fff' }}>
+        {`Usuário Logado: ${usuarioLogado}`}
+      </span>
     </Nav>
   );
 }
