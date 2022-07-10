0818f8a7e6c3fc5ec2bd9c386642fa220e1a8c85
stefanyds <ste.stefany22@gmail.com>
2022-07-10 12:31:09 -0300

    feat: added redux-persist

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,24 @@
 import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import Header from './components/Header';
 import AppRoutes from './routes';
 import GlobalStyles from './styles/GlobalStyles';
 import history from './services/history';
-import store from './store';
+import store, { persistor } from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <HistoryRouter history={history}>
-        <Header />
-        <AppRoutes />
-        <GlobalStyles />
-        <ToastContainer autoClose={3000} />
-      </HistoryRouter>
+      <PersistGate persistor={persistor}>
+        <HistoryRouter history={history}>
+          <Header />
+          <AppRoutes />
+          <GlobalStyles />
+          <ToastContainer autoClose={3000} />
+        </HistoryRouter>
+      </PersistGate>
     </Provider>
   );
 }
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,17 @@
 import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { persistStore } from 'redux-persist';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
+import persistedReducers from './modules/reduxPersist';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const store = createStore(
+  persistedReducers(rootReducer),
+  applyMiddleware(sagaMiddleware)
+);
 sagaMiddleware.run(rootSaga);
 
 export default store;
+export const persistor = persistStore(store);
