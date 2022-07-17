6c68d85b9b63b183b6aa75fcf1033f4f9b58db17
stefanyds <ste.stefany22@gmail.com>
2022-07-17 12:49:33 -0300

    feat: get students data from remote api

--- frontend/src/pages/Students/index.jsx
+++ frontend/src/pages/Students/index.jsx
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
 import { Container } from '../../styles/GlobalStyles';
+import axios from '../../services/axios';
 
 export default function Students() {
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get('/students');
+      console.log(response.data);
+    };
+
+    getData();
+  }, []);
   return (
     <Container>
       <h1>Listagem de Alunos</h1>
