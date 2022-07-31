158baace4c11fe4891e1066c474f3b15458c7d85
stefanyds <ste.stefany22@gmail.com>
2022-07-31 12:24:30 -0300

    feat: created students list page

--- frontend/src/pages/Students/index.jsx
+++ frontend/src/pages/Students/index.jsx
@@ -1,12 +1,18 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { get } from 'lodash';
+import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import { Container } from '../../styles/GlobalStyles';
 import axios from '../../services/axios';
+import { StudentContainer, ProfilePicture } from './styled';
 
 export default function Students() {
+  const [students, setStudents] = useState([]);
+
   useEffect(() => {
     const getData = async () => {
       const response = await axios.get('/students');
-      console.log(response.data);
+      setStudents(response.data);
     };
 
     getData();
@@ -14,6 +20,31 @@ export default function Students() {
   return (
     <Container>
       <h1>Listagem de Alunos</h1>
+
+      <StudentContainer>
+        {students.map((student) => (
+          <div key={student.id}>
+            <ProfilePicture>
+              {get(student, 'Photos[0].url', false) ? (
+                <img src={student.Photos[0].url} alt="Foto do aluno" />
+              ) : (
+                <FaUserCircle size={36} />
+              )}
+            </ProfilePicture>
+            <span className="info">
+              <span className="name">{student.name}</span>
+              <span>{student.email}</span>
+            </span>
+
+            <Link to={`/student/${student.id}/edit`}>
+              <FaEdit size={16} />
+            </Link>
+            <Link to={`/student/${student.id}/delete`}>
+              <FaWindowClose size={16} />
+            </Link>
+          </div>
+        ))}
+      </StudentContainer>
     </Container>
   );
 }
--- frontend/src/pages/Students/styled.js
+++ frontend/src/pages/Students/styled.js
@@ -1,3 +1,40 @@
 import styled from 'styled-components';
 
-export const Paragraph = styled.p``;
+export const StudentContainer = styled.div`
+  margin-top: 1.25rem;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0.3125rem 0;
+
+    .info {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      width: 80%;
+      padding-left: 10px;
+    }
+
+    span {
+      font-size: small;
+    }
+
+    .name {
+      font-size: medium;
+      font-weight: bold;
+    }
+  }
+
+  div + div {
+    border-top: 0.0625rem solid #eee;
+  }
+`;
+export const ProfilePicture = styled.div`
+  img {
+    width: 2.25rem;
+    height: 2.25rem;
+    border-radius: 50%;
+  }
+`;
