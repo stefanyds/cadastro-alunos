949711862a21cf6b3ca96fb06b12f9f34224b622
stefanyds <ste.stefany22@gmail.com>
2022-08-07 10:20:32 -0300

    feat: User page form validators

--- frontend/src/pages/User/index.jsx
+++ frontend/src/pages/User/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { toast } from 'react-toastify';
 import { get } from 'lodash';
+import { isEmail } from 'validator';
 import axios from '../../services/axios';
 import { Container } from '../../styles/GlobalStyles';
 import { Form } from './styled';
@@ -12,6 +13,23 @@ export default function User() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    let formErrors = false;
+
+    if (name.length < 3 || name.length > 255) {
+      formErrors = true;
+      toast.error('Nome deve ter entre 3 e 255 caracteres');
+    }
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.error('Email inválido');
+    }
+    if (password.length < 6 || password.length > 12) {
+      formErrors = true;
+      toast.error('Senha deve conter de 6 a 12 caracteres');
+    }
+    if (formErrors) {
+      return;
+    }
     try {
       const response = await axios.post('/users', { name, email, password }); // endereço do backend
       toast.info(`${response.data.name} foi cadastrado com sucesso`);
