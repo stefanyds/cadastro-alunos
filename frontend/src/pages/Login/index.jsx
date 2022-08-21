f28a1d5a6b621c94e044c663219a5fc40772905a
stefanyds <ste.stefany22@gmail.com>
2022-08-21 11:19:50 -0300

    feat: created Login page

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,9 +1,48 @@
+import { useState } from 'react';
+import isEmail from 'validator/lib/isEmail';
+import { toast } from 'react-toastify';
 import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let formErrors = false;
+
+    if (isEmail(email) === false) {
+      formErrors = true;
+      toast.error('Email inválido');
+    }
+
+    if (password.length < 6 || password.length > 12) {
+      formErrors = true;
+      toast.error('Senha deve conter de 6 a 12 caracteres');
+    }
+    if (formErrors) return;
+    toast.success('Validação do formulário de Login efetuada com sucesso!');
+  };
+
   return (
     <Container>
       <h1>Login</h1>
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Seu email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Sua senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Entrar</button>
+      </Form>
     </Container>
   );
 }
--- frontend/src/pages/Login/styled.js
+++ frontend/src/pages/Login/styled.js
@@ -1,3 +1,21 @@
 import styled from 'styled-components';
+import * as colors from '../../styles/colors';
 
-export const Paragraph = styled.p``;
+export const Form = styled.form`
+  margin-top: 1.25rem;
+  display: flex;
+  flex-direction: column;
+
+  input {
+    margin-bottom: 1.25rem;
+    height: 2.5rem;
+    font-size: 1.125rem;
+    border-radius: 0.3125rem;
+    border: 0.0625rem solid #ddd;
+    padding: 0 0.625rem;
+
+    &:focus {
+      border: 0.0625rem solid ${colors.primaryColor};
+    }
+  }
+`;
