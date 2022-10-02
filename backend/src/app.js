e83bee01a6def79a5d3aa5aa3a8e237d49ea5892
stefanyds <ste.stefany22@gmail.com>
2022-10-02 10:53:55 -0300

    feat: added express-delay for validate Loading component in frontend module

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { resolve } from 'path';
 import './database';
 import cors from 'cors';
+import delay from 'express-delay';
 import homeRoutes from './routes/homeRouter';
 import jwtTokenRoutes from './routes/jwtTokenRouter';
 import photoRoutes from './routes/photoRouter';
@@ -22,6 +23,7 @@ class App {
     this.app.use(express.json());
     this.app.use(express.static(resolve(process.env.STATIC_DIR, 'uploads')));
     this.app.use(cors());
+    this.app.use(delay(3000));
   }
 
   routes() {
