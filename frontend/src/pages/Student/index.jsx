b13d54dfb11a7d91abca66410b6d86c751e80960
stefanyds <ste.stefany22@gmail.com>
2022-11-06 11:55:11 -0300

    feat: added edit and create features

--- frontend/src/pages/Student/index.jsx
+++ frontend/src/pages/Student/index.jsx
@@ -1,9 +1,14 @@
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { get } from 'lodash';
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { isEmail, isInt, isFloat } from 'validator';
+import { useDispatch } from 'react-redux';
+import Loading from '../../components/Loading';
+import axios from '../../services/axios';
 import { Container } from '../../styles/GlobalStyles';
 import { Form } from './styled';
+import * as actions from '../../store/modules/login/actions';
 
 export default function Student() {
   const { id } = useParams();
@@ -13,8 +18,44 @@ export default function Student() {
   const [age, setAge] = useState('');
   const [weight, setWeight] = useState('');
   const [height, setHeight] = useState('');
+  const [isLoading, setIsloading] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const handleSubmit = (event) => {
+  useEffect(() => {
+    if (!id) return;
+
+    const getData = async () => {
+      try {
+        setIsloading(true);
+        const { data } = await axios.get(`/students/${id}`);
+        setName(data.name);
+        setLastname(data.lastname);
+        setEmail(data.email);
+        setAge(data.age);
+        setWeight(data.weight);
+        setHeight(data.height);
+      } catch (error) {
+        const errors = get(error, 'response.data.errors', []);
+        const status = get(error, 'response.status', 0);
+
+        if (status === 401) {
+          toast.error('Você precisa fazer login!');
+          dispatch(actions.doLoginFailure());
+          navigate('/login');
+        } else {
+          errors.forEach((e) => toast.error(e));
+          navigate('/');
+        }
+      } finally {
+        setIsloading(false);
+      }
+    };
+    getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
     let formErrors = false;
 
@@ -33,24 +74,28 @@ export default function Student() {
       toast.error('E-mail inválido');
     }
 
-    if (!isInt(age) || parseInt(age, 10) <= 0 || parseInt(age, 10) > 120) {
+    if (
+      !isInt(age.toString()) ||
+      parseInt(age.toString(), 10) <= 0 ||
+      parseInt(age.toString(), 10) > 120
+    ) {
       formErrors = true;
       toast.error('Idade inválida');
     }
 
     if (
-      !isFloat(weight) ||
-      parseFloat(weight) <= 0 ||
-      parseFloat(weight) > 150
+      !isFloat(weight.toString()) ||
+      parseFloat(weight.toString()) <= 0 ||
+      parseFloat(weight.toString()) > 150
     ) {
       formErrors = true;
       toast.error('Peso inválido');
     }
 
     if (
-      !isFloat(height) ||
-      parseFloat(height) <= 0 ||
-      parseFloat(height) > 2.4
+      !isFloat(height.toString()) ||
+      parseFloat(height.toString()) <= 0 ||
+      parseFloat(height.toString()) > 2.4
     ) {
       formErrors = true;
       toast.error('Altura inválida');
@@ -59,11 +104,54 @@ export default function Student() {
     // eslint-disable-next-line no-useless-return
     if (formErrors) return;
 
-    toast.success('Cadastro válido');
+    try {
+      setIsloading(true);
+      if (id) {
+        // edição
+        await axios.put(`/students/${id}`, {
+          name,
+          lastname,
+          email,
+          age,
+          weight,
+          height,
+        });
+
+        toast.success('Aluno(a) editado(a) com sucesso!');
+      } else {
+        // criação
+        await axios.post(`/students`, {
+          name,
+          lastname,
+          email,
+          age,
+          weight,
+          height,
+        });
+
+        toast.succes('Aluno(a) criado(a) com sucesso!');
+      }
+      navigate('/');
+    } catch (error) {
+      const errors = get(error, 'response.data.errors', []);
+      const status = get(error, 'response.status', 0);
+
+      if (status === 401) {
+        toast.error('Você precisa fazer login!');
+        dispatch(actions.doLoginFailure());
+        navigate('/login');
+      } else {
+        errors.forEach((e) => toast.error(e));
+        navigate('/');
+      }
+    } finally {
+      setIsloading(false);
+    }
   };
 
   return (
     <Container>
+      <Loading isLoading={isLoading} />
       <h1>{id ? 'Editar aluno' : 'Novo Aluno'}</h1>
       <Form onSubmit={handleSubmit}>
         <input
